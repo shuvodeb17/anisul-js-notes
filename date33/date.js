7ba292086910d6d1c66e33ceaf17718f05fa9e1d
shuvodeb17 <shuvodeb17014@gmail.com>
2022-07-20 10:14:20 +0600

    date folder complate

--- date33/date.js
+++ date33/date.js
@@ -1,2 +1,30 @@
 // Class-33
 // Date object and its method
+
+var date = new Date();
+console.log(date);
+
+var currentDay = date.getDay();
+console.log(currentDay);
+
+var year = date.getFullYear();
+console.log(year);
+
+var month = date.getMonth();
+console.log(month);
+
+var currentDate = date.getDate();
+console.log(currentDate);
+
+var currentHour = date.getHours();
+console.log(currentHour);
+
+var minutes = date.getMinutes();
+console.log(minutes);
+
+var currentSecond = date.getSeconds();
+console.log(currentSecond);
+
+var getMiliSecond = date.getMilliseconds();
+console.log(getMiliSecond);
+
