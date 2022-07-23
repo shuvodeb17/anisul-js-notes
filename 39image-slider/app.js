1e1ab8a2b6069cd1e85e8a540376f76ebcf870ad
shuvodeb17 <shuvodeb17014@gmail.com>
2022-07-23 16:35:44 +0600

    39 image-slider add

---
+++ 39image-slider/app.js
@@ -0,0 +1,23 @@
+var imgTag = document.getElementById('imgTag');
+var images = ['img/1.jpeg', 'img/2.jpg', 'img/3.jpg'];
+var count = 0;
+
+function next() {
+    count++;
+    if (count >= images.length) {
+        count = 0;
+        imgTag.src = images[count];
+    } else {
+        imgTag.src = images[count];
+    }
+}
+
+function previous() {
+    count--;
+    if (count < 0) {
+        count = images.length - 1;
+        imgTag.src = images[count];
+    } else {
+        imgTag.src = images[count];
+    }
+}
