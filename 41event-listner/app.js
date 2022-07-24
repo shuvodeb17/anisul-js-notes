7778e8ed6868fc37cf27e361b0efad8b0c260290
shuvodeb17 <shuvodeb17014@gmail.com>
2022-07-24 13:41:05 +0600

    41 folder update

--- 41event-listner/app.js
+++ 41event-listner/app.js
@@ -1,4 +1,8 @@
 var heading = document.getElementById('head');
-heading.addEventListener('click', function(){
+heading.addEventListener('mouseover', function(){
     heading.classList.toggle('add-style');
+});
+
+heading.addEventListener('mouseout', function(){
+    heading.classList.add('add-style');
 });
