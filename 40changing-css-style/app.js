a4297336b07242641d654bf0eb713007b757fd59
shuvodeb17 <shuvodeb17014@gmail.com>
2022-07-24 13:37:57 +0600

    adding 40-41

---
+++ 40changing-css-style/app.js
@@ -0,0 +1,8 @@
+var heading = document.getElementById('heading');
+function add(){
+    heading.classList.add('adding');
+    
+}
+function remove(){
+    heading.classList.remove('adding');
+}
---
+++ 41event-listner/app.js
@@ -0,0 +1,4 @@
+var heading = document.getElementById('head');
+heading.addEventListener('click', function(){
+    heading.classList.toggle('add-style');
+});
